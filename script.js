59d9d44da6bd9d0bd8f428ed993d633b459e6185
ConnerKT <connerthompson121@gmail.com>
2023-02-15 12:54:37 -0600

    Test

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-function dark {
+
+
+function dark() {
+   let currentmode = document.getElementById("dark").getAttribute("src")
+   if (currentmode =="icons/lightmode.png"){
+        document.getElementById("dark").setAttribute("src", "icons/darkmode.png");
+   }else {
+        document.getElementById("dark").setAttribute("src", "icons/lightmode.png");
+   }
+   var elements = document.body.getElementsByTagName("*");
+   console.log(elements)
+    for (let x = 0; x < elements.length; x++){
+        let element = elements[x]
+        if (element.tagName == "HEADER"){
+            
+            let color = element.style.color
+             console.log(element.style.color)
+             console.log(document.getElementsByTagName("header"))
+            if (color == "white"){
+                elements.style.color=("black");
+            }
+        }
+        
+        
+            
+
+    }
+
     
-}sccx
+}
+

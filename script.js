415febd0670290753e889dcad5b2a8042e75e291
ConnerKT <connerthompson121@gmail.com>
2023-02-18 18:19:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,18 +8,22 @@ function dark() {
         document.getElementById("dark").setAttribute("src", "icons/lightmode.png");
    }
    var elements = document.getElementsByTagName("*");
-   
+   var bgcolor = document.body
     for (let x = 0; x < elements.length; x++){
         //grab element from array 
         let element = elements[x]
 
         //grab element's CSS values using built-in function
         let elementStyle = getComputedStyle(element)    //Beware! this variable is a only place holder for an element's CSS values.
-                                                        //Changing its values directly (elementStyle.color = black) wont have any affect on the element itself!
-
-        console.log(element.tagName)
-        console.log(elementStyle.color)
-        console.log(elementStyle.backgroundColor)
+        
+        if (elementStyle.color === "rgb(0, 0, 0)"){
+            element.style.color = "rgb(255,255,255)";
+        }else{
+            element.style.color = "rgb(0, 0, 0)";
+        }
+        
+        
+                                                      //Changing its values directly (elementStyle.color = black) wont have any affect on the element itself! 
 
         //how to CHECK a CSS value on an element
         //if (elementStyle.color == "black") {} RIGHT

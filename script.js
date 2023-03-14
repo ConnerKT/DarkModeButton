78ae1b940dbc14c37c94b4b14c3c36dc3d11cd4b
ConnerKT <connerthompson121@gmail.com>
2023-03-14 12:12:52 -0500

    Bug Fix Attempt
    Trying to fix the header tag, implemented some solutions that aren't working.

--- script.js
+++ script.js
@@ -10,28 +10,49 @@ function dark() {
    var elements = document.getElementsByTagName("*");
    var header = document.getElementsByTagName("header")[0];
    var bgcolor = document.body
+
+//    h1tag = document.getElementsByTagName("H1")
+//    while (elements === h1tag){
+//         if (getComputedStyle.h1tag === rgb(0, 0, 0)){
+//             return;
+//         }else if (getComputedStyle.h1tag === rgb(255, 255, 255)){
+//             return;
+//         }
+//     h1tag.style.color = rgb(0, 0, 0)
+        
+  // }    
     for (let x = 0; x < elements.length; x++){
         //grab element from array 
         let element = elements[x]
 
         //grab element's CSS values using built-in function
         let elementStyle = getComputedStyle(element)    //Beware! this variable is a only place holder for an element's CSS values.
-        // console.log("tagname",element.header)
-        // console.log("color",elementStyle.color)
-        // console.log("bgoclor",elementStyle.header)
+       
+        if (element.tagName === "H1"){
+            if (elementStyle.color === "rgb(0, 0, 0)"){
+                break;
+            }else if (elementStyle.color === "rgb(255, 255, 255)"){
+                break;
+            }else if (elementStyle.color === ""){
+                element.style.color = "rgb(0, 0, 0)"
+            }
+        break;
+        }
+        
+
         if(element.tagName == "H1"){
             console.log("tagname",element.tagName)
             console.log("color",elementStyle.color)
             console.log("bgoclor",elementStyle.backgroundColor)
             console.log("==============BEFORE CHANGE=================")
         }
-        if(element.tagName == "P"){
-            console.log("tagname",element.tagName)
-            console.log("color",elementStyle.color)
-            console.log("bgoclor",elementStyle.backgroundColor)
-            console.log("==============BEFORE CHANGE=================")
-        }
-   
+
+        // if (element === "H1"){
+        //     if ("H1" === "rgb(0, 0, 0)"){
+        //         return;
+        //     }else
+        //     element.style.color ="rgb(255, 255, 255)"
+        // }
         if (elementStyle.color === "rgb(0, 0, 0)") {
             element.style.color = "rgb(255, 255, 255)";
         }else if(elementStyle.color === "rgb(255, 255, 255)"){
@@ -50,13 +71,7 @@ function dark() {
             console.log("bgoclor",elementStyle.backgroundColor)
             console.log("==============AFTER CHANGE=================")
         }
-        if(element.tagName == "P"){
-            console.log("tagname",element.tagName)
-            console.log("color",elementStyle.color)
-            console.log("bgoclor",elementStyle.backgroundColor)
-            console.log("==============AFTER CHANGE=================")
-        }
-                                                      //Changing its values directly (elementStyle.color = black) wont have any affect on the element itself! 
+                                           //Changing its values directly (elementStyle.color = black) wont have any affect on the element itself! 
 
         //how to CHECK a CSS value on an element
         //if (elementStyle.color == "black") {} RIGHT

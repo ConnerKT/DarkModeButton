9625385bd522b0da47b09301d72afc0b3c08ce73
ConnerKT <connerthompson121@gmail.com>
2023-03-14 22:18:01 -0500

    Cleanup
    Cleaned up code, removed empty code

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 function dark() {
    let currentmode = document.getElementById("dark").getAttribute("src")
    if (currentmode =="icons/lightmode.png"){
@@ -11,43 +9,19 @@ function dark() {
    var header = document.getElementsByTagName("header")[0];
    var bgcolor = document.body
 
-//    h1tag = document.getElementsByTagName("H1")
-//    while (elements === h1tag){
-//         if (getComputedStyle.h1tag === rgb(0, 0, 0)){
-//             return;
-//         }else if (getComputedStyle.h1tag === rgb(255, 255, 255)){
-//             return;
-//         }
-//     h1tag.style.color = rgb(0, 0, 0)
-        
-  // }    
     for (let x = 0; x < elements.length; x++){
         //grab element from array 
         let element = elements[x]
 
         //grab element's CSS values using built-in function
-        let elementStyle = getComputedStyle(element)    //Beware! this variable is a only place holder for an element's CSS values.
+        let elementStyle = getComputedStyle(element)    
        
         if (element.tagName === "H1" || element.tagName === "H2" || element.tagName === "H3" || element.tagName === "H4" || element.tagName === "H5" || element.tagName === "H6"){
             if (elementStyle.color === "rgb(0, 0, 0)" || elementStyle.color === "rgb(255, 255, 255)"){
                 continue;
             }
         }
-        
 
-        if(element.tagName == "H1"){
-            console.log("tagname",element.tagName)
-            console.log("color",elementStyle.color)
-            console.log("bgoclor",elementStyle.backgroundColor)
-            console.log("==============BEFORE CHANGE=================")
-        }
-
-        // if (element === "H1"){
-        //     if ("H1" === "rgb(0, 0, 0)"){
-        //         return;
-        //     }else
-        //     element.style.color ="rgb(255, 255, 255)"
-        // }
         if (elementStyle.color === "rgb(0, 0, 0)") {
             element.style.color = "rgb(255, 255, 255)";
         }else if(elementStyle.color === "rgb(255, 255, 255)"){
@@ -58,27 +32,6 @@ function dark() {
         }else if(elementStyle.backgroundColor === "rgb(255, 255, 255)"){
             element.style.backgroundColor = "rgb(0, 0, 0)";
         }
-
-        
-        if(element.tagName == "H1"){
-            console.log("tagname",element.tagName)
-            console.log("color",elementStyle.color)
-            console.log("bgoclor",elementStyle.backgroundColor)
-            console.log("==============AFTER CHANGE=================")
-        }
-                                           //Changing its values directly (elementStyle.color = black) wont have any affect on the element itself! 
-
-        //how to CHECK a CSS value on an element
-        //if (elementStyle.color == "black") {} RIGHT
-        //if (element.style.color == "black") {} WRONG (This is how we were doing it yesterday.)
-        
-        //how to CHANGE a CSS value on an element
-        // element.style.color = "black"   RIGHT
-        // elementStyle.color = "black"    WRONG
-
-
-    }
-
-    
+    }   
 }
 
